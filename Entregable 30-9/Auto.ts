e36ea7f30f4e4e4dc296dbb4f7a2d1039275f94c
Maico Olguin <168041229+cuentahitgub1@users.noreply.github.com>
2024-10-02 19:38:09 -0300

    Auto.ts

--- Entregable 30-9/Auto.ts
+++ Entregable 30-9/Auto.ts
@@ -1,13 +1,60 @@
 export class Auto {
-    constructor(
-        public marca: string,
-        public modelo: string,
-        public año: number,
-        public color: string,
-        public ruedas: string,
-        public patente: string,
-    ) {}
+    private marca: string;
+    private modelo: string;
+    private año: number;
+    private color: string;
+    private ruedas: string;
+    private patente: string;
+   
+    constructor(marca: string, modelo: string, año: number, color: string, ruedas: string, patente: string) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+        this.ruedas = ruedas;
+        this.patente = patente;
+    }
 
+    //Getter
+    public getMarca(): string {
+        return this.marca;
+    }
+    public getModelo(): string {
+        return this.modelo;
+    }
+    public getAño(): number {
+        return this.año;
+    }
+    public getColor(): string {
+        return this.color;
+    }
+    public getRuedas(): string {
+        return this.ruedas;
+    }
+    public getPatente(): string {
+        return this.patente;
+    }
+
+    //Setter
+    public setMarca(marca: string): void {
+        this.marca = marca;
+    }
+    public setModelo(modelo: string): void {
+        this.modelo = modelo;
+    }
+    public setAño(año: number): void {
+        this.año = año;
+    }
+    public setColor(color: string): void {
+        this.color = color;
+    }
+    public setRuedas(ruedas: string): void {
+        this.ruedas = ruedas;
+    }
+    public setPatente(patente: string): void {
+        this.patente = patente;
+    }
+    
     public toString(): string {
         return `Auto: ${this.marca} ${this.modelo} (${this.año}), Color: ${this.color}, Ruedas: ${this.ruedas}, Patente: ${this.patente}`;
     }
